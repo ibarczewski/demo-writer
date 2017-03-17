5b8def2dd546dcdcf22e1731e0149f27c9cba6f6
Ian Barczewski <mercy-virtual+ib@asynchrony.com>
2017-03-17 11:05:06 -0500

    cleaned up code

--- src/app/services/collections-service.ts
+++ src/app/services/collections-service.ts
@@ -1,9 +1,16 @@
 import {Injectable} from "@angular/core";
+import {Http, Response} from "@angular/http";
+import {Collection} from "../models/collection";
+import {Observable} from "rxjs";
+import 'rxjs/Rx';
 @Injectable()
 export class CollectionsService {
-  constructor(){}
+  constructor(private http: Http){}
 
-  getCollections() {
+  getCollections(): Observable<Array<Collection>> {
+    return this.http.get('http://localhost:3000/collections').map((collection) => {
+      return collection.json().collections;
+    });
 
   }
 }
